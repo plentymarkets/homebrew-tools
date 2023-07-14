f5ed2d359c4d4871886b374a0d59cf3b016cb2a3
gvoiculescu-plenty <george.voiculescu@plentymarkets.com>
2023-07-14 18:53:09 +0300

    Brew formula update for plenty version v1.0.16

--- plenty.rb
+++ plenty.rb
@@ -5,7 +5,7 @@
 class Plenty < Formula
   desc ""
   homepage "https://github.com/plentymarkets/homebrew-tools"
-  version "1.0.15"
+  version "1.0.16"
 
   depends_on "gh"
   depends_on "jq"
@@ -15,16 +15,16 @@ class Plenty < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "http://brew-proxy.eks-114-1.plenty.rocks/plenty/v1.0.15/plenty_1.0.15_Darwin_arm64.tar.gz"
-      sha256 "db89e8fb530e51d8c02203dfef744188cfc1f1617ece9d8ceee799d52c198600"
+      url "http://brew-proxy.eks-114-1.plenty.rocks/plenty/v1.0.16/plenty_1.0.16_Darwin_arm64.tar.gz"
+      sha256 "a19fac8f0e1d82afe2d8e15c33b7540e043fdb0f9baec5e81093a04eafdf0ffc"
 
       def install
         bin.install "plenty"
       end
     end
     if Hardware::CPU.intel?
-      url "http://brew-proxy.eks-114-1.plenty.rocks/plenty/v1.0.15/plenty_1.0.15_Darwin_amd64.tar.gz"
-      sha256 "b0581e36fda953a9eb653d9f145ff2d984740ca3137e7ae4bb521276d2ea57da"
+      url "http://brew-proxy.eks-114-1.plenty.rocks/plenty/v1.0.16/plenty_1.0.16_Darwin_amd64.tar.gz"
+      sha256 "1e8e9e6c3a62440f841124beff6e7fae4f68f39f72f1c7ea36561a1bedca6d6e"
 
       def install
         bin.install "plenty"
